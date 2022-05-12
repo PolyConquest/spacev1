72209150e59e4f1b29f0bfdf03ccf5cdcc7ca048
Sebastien-Gineste <47150778+Sebastien-Gineste@users.noreply.github.com>
2022-05-12 09:19:49 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// -------------------------------
+// Fichier généré automatiquement par polyconquest :
+// Veuillez utiliser ce fichier pour envoyer votre code
+// -------------------------------
+let graph = JSON.parse(readLine())
+console.log("move 0 1")
